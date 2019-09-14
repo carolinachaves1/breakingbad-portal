7812f3fe522c7129c1b5f9d933e9971ec4ca82bc
Carolina Chaves <carolina.amaralchaves@gmail.com>
2019-09-14 16:45:59 -0300

    Updates

---
+++ precache-manifest.3b41cad65137dad67825cfff6fe7098d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b94a2fb2dac7c36173845f2fd743b80",
+    "url": "/breakingbad-portal/index.html"
+  },
+  {
+    "revision": "352315f31879e5c49f63",
+    "url": "/breakingbad-portal/static/css/main.e1a0d8c2.chunk.css"
+  },
+  {
+    "revision": "7858bd52871443103234",
+    "url": "/breakingbad-portal/static/js/2.5b699a36.chunk.js"
+  },
+  {
+    "revision": "352315f31879e5c49f63",
+    "url": "/breakingbad-portal/static/js/main.8b4779f4.chunk.js"
+  },
+  {
+    "revision": "e6f30b60bc989cb00e87",
+    "url": "/breakingbad-portal/static/js/runtime~main.de132d22.js"
+  },
+  {
+    "revision": "94e1d840b091f01c3dda655d92162944",
+    "url": "/breakingbad-portal/static/media/unnamed.94e1d840.jpg"
+  }
+]);
